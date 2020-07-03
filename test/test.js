2b3905fb6b886fdff320bcf2dbcbf9723a75d648
Mitchell Cash <mitchell@mitchellcash.com>
2020-07-03 09:35:05 +0100

    Add a GitHub Workflow CI pipeline

--- test/test.js
+++ test/test.js
@@ -23,35 +23,35 @@ describe('Password comparison tests', () => {
     });
   });
 
-    test('crypt_blowfish hashed password (version $2y$) is pwned should return true', async () => {
-        const plainTextPassword = 'CorrectHorseBatteryStaple';
-        const hashedPassword = '$2y$10$8zuUHLLLrbgdZMLZ.X0.6.5O8tdHdVo0hTjiwmmTqk8ZTsZoWFzEq';
-        await isPasswordPwned(plainTextPassword, hashedPassword, (res) => {
-            expect(res).toBe(true)
-        });
+  test('crypt_blowfish hashed password (version $2y$) is pwned should return true', async () => {
+    const plainTextPassword = 'CorrectHorseBatteryStaple';
+    const hashedPassword = '$2y$10$8zuUHLLLrbgdZMLZ.X0.6.5O8tdHdVo0hTjiwmmTqk8ZTsZoWFzEq';
+    await isPasswordPwned(plainTextPassword, hashedPassword, (res) => {
+      expect(res).toBe(true)
     });
+  });
 
-    test('crypt_blowfish hashed password (version $2y$) is not pwned should return false', async () => {
-        const plainTextPassword = '12345678';
-        const hashedPassword = '$2y$10$8zuUHLLLrbgdZMLZ.X0.6.5O8tdHdVo0hTjiwmmTqk8ZTsZoWFzEq';
-        await isPasswordPwned(plainTextPassword, hashedPassword, (res) => {
-            expect(res).toBe(false)
-        });
+  test('crypt_blowfish hashed password (version $2y$) is not pwned should return false', async () => {
+    const plainTextPassword = '12345678';
+    const hashedPassword = '$2y$10$8zuUHLLLrbgdZMLZ.X0.6.5O8tdHdVo0hTjiwmmTqk8ZTsZoWFzEq';
+    await isPasswordPwned(plainTextPassword, hashedPassword, (res) => {
+      expect(res).toBe(false)
     });
+  });
 
-    test('OpenBSD bcrypt hashed password (version $2b$) is pwned should return true', async () => {
-        const plainTextPassword = 'CorrectHorseBatteryStaple';
-        const hashedPassword = '$2b$10$8zuUHLLLrbgdZMLZ.X0.6.5O8tdHdVo0hTjiwmmTqk8ZTsZoWFzEq';
-        await isPasswordPwned(plainTextPassword, hashedPassword, (res) => {
-            expect(res).toBe(true)
-        });
+  test('OpenBSD bcrypt hashed password (version $2b$) is pwned should return true', async () => {
+    const plainTextPassword = 'CorrectHorseBatteryStaple';
+    const hashedPassword = '$2b$10$8zuUHLLLrbgdZMLZ.X0.6.5O8tdHdVo0hTjiwmmTqk8ZTsZoWFzEq';
+    await isPasswordPwned(plainTextPassword, hashedPassword, (res) => {
+      expect(res).toBe(true)
     });
+  });
 
-    test('OpenBSD bcrypt hashed password (version $2b$) is not pwned should return false', async () => {
-        const plainTextPassword = '12345678';
-        const hashedPassword = '$2b$10$8zuUHLLLrbgdZMLZ.X0.6.5O8tdHdVo0hTjiwmmTqk8ZTsZoWFzEq';
-        await isPasswordPwned(plainTextPassword, hashedPassword, (res) => {
-            expect(res).toBe(false)
-        });
+  test('OpenBSD bcrypt hashed password (version $2b$) is not pwned should return false', async () => {
+    const plainTextPassword = '12345678';
+    const hashedPassword = '$2b$10$8zuUHLLLrbgdZMLZ.X0.6.5O8tdHdVo0hTjiwmmTqk8ZTsZoWFzEq';
+    await isPasswordPwned(plainTextPassword, hashedPassword, (res) => {
+      expect(res).toBe(false)
     });
+  });
 });
